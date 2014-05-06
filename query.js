463164b1d12d6811b9d5e28c1fa65b3b78697489
Anders Riutta <git@andersriutta.com>
2014-05-05 21:05:22 -0700

    successfully running multiple queries on this thing

--- query.js
+++ query.js
@@ -5,35 +5,46 @@ var ldf = require('ldf-client'),
       N3 = require('n3');
 // Create the client to solve the query
 
-var config = {
-  "datasource": "http://localhost:5000/fcf",
-  "prefixes": {
-    "rdf":         "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-    "gpml":     "https://wikipathways.firebaseio.com/.json"
-  }
-};
+function runQuery(thisPathwayNumber) {
+  console.log(thisPathwayNumber);
+  var pathwayNumberAsLetters = String(thisPathwayNumber).split('').map(function(number) {
+    return String.fromCharCode(number % 26 + 97);
+  }).join('');
 
-var query = 'SELECT * WHERE { ?s <http://www.biopax.org/release/biopax-level3.owl#UnificationXref> ?o } LIMIT 10';
+  var config = {
+    "datasource": "http://localhost:5000/" + pathwayNumberAsLetters,
+    "prefixes": {
+      "rdf":         "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+      "gpml":     "https://wikipathways.firebaseio.com/.json"
+    }
+  };
 
+  var query = 'SELECT * WHERE { ?s <http://www.biopax.org/release/biopax-level3.owl#UnificationXref> ?o } LIMIT 10';
 
-      var client = new LinkedDataFragmentsClient(config),
-          querySolver = new SparqlQuerySolver(client, config.prefixes);
-      querySolver.getQueryResults(query)
-      .then(function (result) {
-        // Display result rows
-        if (result.rows) {
-          console.log(JSON.stringify(result.rows, null, '  '));
-        }
-        // Display Turtle
-        else {
-          var writer = new N3.Writer(config.prefixes);
-          writer.addTriples(result.triples);
-          writer.end(function (error, turtle) {
-            // Display error or Turtle result
-            if (error) return console.log(error);
-            console.log(turtle);
-          });
-        }
-      })
-      .fail(function (error) { console.log(error.message); })
-      .fin(function () { $execute.prop('disabled', false); });
+  var client = new LinkedDataFragmentsClient(config),
+      querySolver = new SparqlQuerySolver(client, config.prefixes);
+  querySolver.getQueryResults(query)
+    .then(function (result) {
+      // Display result rows
+      if (result.rows) {
+        console.log(JSON.stringify(result.rows, null, '  '));
+      }
+      // Display Turtle
+      else {
+        var writer = new N3.Writer(config.prefixes);
+        writer.addTriples(result.triples);
+        writer.end(function (error, turtle) {
+          // Display error or Turtle result
+          if (error) return console.log(error);
+          console.log(turtle);
+        });
+      }
+    })
+  .fail(function (error) { console.log(error.message); })
+  .fin(function () { $execute.prop('disabled', false); });
+}
+
+var pathwayNumbers = [525, 1203];
+pathwayNumbers.forEach(function(pathwayNumber) {
+  runQuery(pathwayNumber);
+});
